d780f3bec394f9c85a880ccc139bff1c6f01a2f0
Jake Thomas <jmaxt12@gmail.com>
2019-07-16 15:08:17 -0600

    added rules and minor styling

--- life-game/src/App.js
+++ life-game/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Grid from './components/grid/Grid';
-import About from './components/about/About';
+import Grid from "./components/grid/Grid";
+import About from "./components/about/About";
+import Rules from "./components/rules/Rules";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Conway's Game of Life</h1>
       </header>
-      <Grid />
+      <div className = 'topRow'>
+        <Grid />
+        <Rules />
+      </div>
       <About />
     </div>
   );
